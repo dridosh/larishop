b7143c7209a8b5cdc24558dce78b173988afa266
DmitryR <dridosh@gmail.com>
2022-01-10 18:09:54 +0400

    add routing

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
+    Route::get('/basket', 'App\Http\Controllers\MainController@basket')->name('basket');
+    Route::get('/order', 'App\Http\Controllers\MainController@order')->name('order');
+    Route::get('/categories', 'App\Http\Controllers\MainController@categories')->name('categories');
+    Route::get('/{category}', 'App\Http\Controllers\MainController@category')->name('category');
+    Route::get('/{category}/{product?}', 'App\Http\Controllers\MainController@product')->name('product');
+
