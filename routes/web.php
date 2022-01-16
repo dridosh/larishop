c5b901e9b3b8f4200fa1fac16a8446ad851e19fb
DmitryR <dridosh@gmail.com>
2022-01-16 13:18:17 +0400

    install laravel/ui
    https://larainfo.com/blogs/laravel-8-authentication-with-laravel-ui
    
    php artisan ui bootstrap --auth
    //php artisan ui vue --auth
    //php artisan ui react --auth
    
    npm install && npm run dev
    
    php artisan migrate

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,23 @@
 
     use Illuminate\Support\Facades\Route;
 
+    Auth::routes();
 
     Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
+
     Route::get('/basket', 'App\Http\Controllers\BasketController@basket')->name('basket');
     Route::get('/basket/order', 'App\Http\Controllers\BasketController@order')->name('order');
     Route::post('/basket/order', 'App\Http\Controllers\BasketController@orderConfirm')->name('order-confirm');
     Route::post('/basket/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
     Route::post('/basket/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
 
-
     Route::get('/categories', 'App\Http\Controllers\MainController@categories')->name('categories');
-
     Route::get('/{category}', 'App\Http\Controllers\MainController@category')->name('category');
     Route::get('/{category}/{product?}', 'App\Http\Controllers\MainController@product')->name('product');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
